a057da7831eeea854816ece20a7583b9b39c13cd
Rusu-Desktop <sebastianrusu01@gmail.com>
2025-04-13 16:10:55 +0300

    Implements user authentication feature
    Adds login and registration endpoints
    Updates user model to include password hashing
    Integrates JWT for secure token generation
    
    Fixes #789

--- ezair/src/main/java/com/proiect/PaginaPrincipalaAdminController.java
+++ ezair/src/main/java/com/proiect/PaginaPrincipalaAdminController.java
@@ -6,12 +6,39 @@ public class PaginaPrincipalaAdminController {
     
     @FXML
     private void zboruriAdmin() throws Exception {
-        
+        App.setRoot("paginaZboruriAdmin");
     }
 
     @FXML
     private void useriAdmin() throws Exception {
+        App.setRoot("paginaUseriAdmin");
+    }
+    @FXML
+    private void adaugareZbor() throws Exception {
         
     }
-
+    @FXML
+    private void adaugareUser() throws Exception {
+        
+    }
+    @FXML
+    private void editareZbor() throws Exception {
+        
+    }
+    @FXML
+    private void editareUser() throws Exception {
+        
+    }
+    @FXML
+    private void deleteZbor() throws Exception {
+        
+    }
+    @FXML
+    private void deleteUser() throws Exception {
+        
+    }
+    @FXML
+    private void reload() throws Exception {
+        App.setRoot("paginaPrincipalaAdmin");
+    }
 }
